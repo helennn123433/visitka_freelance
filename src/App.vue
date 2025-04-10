d6a3f6f0c3048f0e29e1e5bf45d82e1a447ce814
Amoyt14 <ZM-HPS200@yandex.ru>
2025-04-10 16:52:07 +0300

    проверка коммита

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,12 @@
 </template>
 
 <script setup lang="ts">
+import SidebarComponent from "@/components/sidebar/SidebarComponent.vue";
+</script>
+=======
+<AboutUs />
+</template>
 
+<script setup lang="ts">
+import AboutUs from './components/aboutUs/AboutUs.vue';
 </script>

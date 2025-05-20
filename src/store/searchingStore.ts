457a07b935e06bd798f40552dc39f7fd704e65be
Vlad Letelov <wladletelov001@gmail.com>
2025-05-20 20:17:06 +0300

    добавлен запуск сервера с несколькими json файлами, обновлен способ получения данных с сервера (api/{nameAPI}).

--- src/store/searchingStore.ts
+++ src/store/searchingStore.ts
@@ -17,7 +17,7 @@ export const useSearchingStore = defineStore('searchingStore', () => {
 
   const fetchServices = async () => {
     try {
-      const response = await axios.get('http://localhost:3004/services');
+      const response = await axios.get('/api/services');
       images.value = response.data;
     } catch (error) {
       console.error('Ошибка загрузки:', error);
